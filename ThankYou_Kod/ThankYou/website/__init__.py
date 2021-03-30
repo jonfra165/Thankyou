a3b0aa4b357d33f590daa3578e22740b09565489
Thippi <37799874+MiuMiuMiuMiuMiu@users.noreply.github.com>
2021-03-30 22:41:35 +0200

    Update __init__.py
    Did not show.

--- ThankYou_Kod/ThankYou/website/__init__.py
+++ ThankYou_Kod/ThankYou/website/__init__.py
@@ -1,13 +1,35 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+import mysql.connector as mariadb
+import pymysql
+
+db = SQLAlchemy()
+db_name = "testdb" #Database name
 
 def create_app():
     app = Flask(__name__) #Initialize flask 
     app.config['SECRET_KEY'] = 'CHOSEN_KEY' #Encrypt/secure cookies and data related to our session
- 
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = f'mariadb+pymysql://root:password@localhost/{db_name}'
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False #Causes an error if not included
+    db.init_app(app) #Initiate db
+
+    #Routes
     from .views import views #From views.py
     from .auth import auth #From auth.py
 
+    #Database tables
+    from .models import user
+
+    #Blueprints
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
+    #No clue, ask Anton or someone else
+    create_database(app)
+
     return app
+
+def create_database(app):
+    db.create_all(app = app)
