d557861b8d478644d3c152b6d42ed3c05b6c31fc
Thippi <37799874+MiuMiuMiuMiuMiu@users.noreply.github.com>
2021-03-30 22:37:46 +0200

    Update __init___.py
    Now able to connect to MariaDB

--- ThankYou_Kod/ThankYou/website/__init__.py
+++ ThankYou_Kod/ThankYou/website/__init__.py
@@ -1,26 +1,17 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-import pyodbc
+from os import path
+import mysql.connector as mariadb
+import pymysql
 
 db = SQLAlchemy()
-db_name = "test_database" #Database name
-
-
-try: #Create database
-    conn = pyodbc.connect("driver={SQL Server};server=localhost; database=master; trusted_connection=true", autocommit=True) 
-    cursor = conn.cursor()
-    cursor.execute(f'CREATE DATABASE {db_name};')
-
-    print(f'Database {db_name} created!')
-except: #SQL ASK Anton or someone else
-    def create_database(app):
-        db.create_all(app = app)
+db_name = "testdb" #Database name
 
 def create_app():
     app = Flask(__name__) #Initialize flask 
     app.config['SECRET_KEY'] = 'CHOSEN_KEY' #Encrypt/secure cookies and data related to our session
 
-    app.config["SQLALCHEMY_DATABASE_URI"] = f'mssql+pyodbc://localhost/{db_name}?driver=SQL+Server?trusted_connection=yes' #No idea what to comment and what it does
+    app.config["SQLALCHEMY_DATABASE_URI"] = f'mariadb+pymysql://root:password@localhost/{db_name}'
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False #Causes an error if not included
     db.init_app(app) #Initiate db
 
@@ -39,3 +30,6 @@ def create_app():
     create_database(app)
 
     return app
+
+def create_database(app):
+    db.create_all(app = app)
