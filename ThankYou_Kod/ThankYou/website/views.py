87aa2f934684be654ab5a39062c42626d2594cf4
Thippi <37799874+MiuMiuMiuMiuMiu@users.noreply.github.com>
2021-04-04 21:12:00 +0200

    Database and login

--- ThankYou_Kod/ThankYou/website/views.py
+++ ThankYou_Kod/ThankYou/website/views.py
@@ -1,13 +1,23 @@
 from flask import Blueprint, render_template, url_for, redirect
+from flask_login import login_required, current_user
+from .models import Note
+from . import db
 
 views = Blueprint('views', __name__)
 
-@views.route('/')
-def sign_up():
-    '''This view redirects to the sign up page'''
-    return redirect(url_for('auth.sign_up'))
-
-@views.route('/home')
+@views.route('/', method=['GET', 'POST'])
+@login_required #User can only see the home page if they are logged in
 def home():
     '''This view returns the home page'''
-    return render_template('home.html')
+    if request.method == 'POST':
+        note = request.form.get('note')
+
+        if len(note) < 1:
+            flash('Note is too short!', category='error')
+        else:
+            new_note = Note(data=note, user_id=current_user.id)
+            db.session.add(new_note)
+            db.session.commit()
+            flash('Note added!', category='success')
+
+    return render_template('home.html', user=current_user)
