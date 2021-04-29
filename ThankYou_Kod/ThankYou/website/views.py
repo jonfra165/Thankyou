e748138bcb8bcc20a01427aa2555a8847d44c0f2
Thippi <37799874+MiuMiuMiuMiuMiu@users.noreply.github.com>
2021-04-29 15:34:55 +0200

    Fix user-note

--- ThankYou_Kod/ThankYou/website/views.py
+++ ThankYou_Kod/ThankYou/website/views.py
@@ -10,7 +10,7 @@ views = Blueprint('views', __name__)
 def home():
     '''This view returns the home page'''
     if request.method == 'POST':
-        note = request.form.get('note')
+        note = request.form.get('note') #Get note from home.html
 
         if len(note) < 1: #If less than one character return an error
             flash('Note is too short!', category='error')
@@ -18,10 +18,10 @@ def home():
             new_note = Note(data=note, user_id=current_user.id)
             db.session.add(new_note)
             db.session.commit()
-            flash('Note added!', category='success')
+            flash('Note added!', category='success') 
     
     user = current_user.id 
-    note = Note.query.all() #get all values from table Note
+    note = Note.query.filter_by(user_id=user).all() #Problemet?
     
     return render_template('home.html', user=user, note=note)
 
@@ -31,3 +31,23 @@ def profile():
     ''' This view returns the profile page '''
     user = current_user
     return render_template("profile.html", user=user)
+
+@views.route("/calendar")
+@login_required
+def calendar():
+    user = current_user
+    return render_template("calendar.html", user=user)
+
+def get_date():
+    '''1 parameter som är ett datum, sen ska funktionen hämta från databasen alla posts som finns på det datumet och skriva ut det i json format
+    Skicka tillbaka i json'''
+
+    test_user = current_user.id 
+    test_note = Note.query.filter_by(user_id=user).all()
+    test_note = list(test_note)
+
+    return render_template('home.html', test_user=test_user, test_note=test_note)
+
+    #Hämta all datum och tillhörande anteckningar
+    #Sparar det i json
+    #Skickar till html.fil
