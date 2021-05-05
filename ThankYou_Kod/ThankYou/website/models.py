bfa061b0a3faca43183310e7c39bd5cc3847881d
jonfra165 <jonna.franzen@hotmail.com>
2021-05-05 11:25:45 +0200

    upload images
    gjort funktionen som där man laddar upp och sparar bilder och inlägg i databasen sen hämtas dom och skrivs ut i calnedar.html.
    La till en ny column i databasen som heter image

--- ThankYou_Kod/ThankYou/website/models.py
+++ ThankYou_Kod/ThankYou/website/models.py
@@ -1,5 +1,5 @@
 #Database models
-from . import db
+from .import db
 from flask_login import UserMixin #UserMixin is used with current_user
 from sqlalchemy.sql import func #Automatically adds date
 
@@ -7,6 +7,7 @@ class Note(db.Model): #Table note
     id = db.Column(db.Integer, primary_key=True)
     data = db.Column(db.String(1000))
     date = db.Column(db.Date, default=func.now())
+    image = db.Column(db.String(500))
     user_id = db.Column(db.Integer, db.ForeignKey('user.id')) #Foreign Key from table 'user' where column is 'id', must be in lower-case
 
 class User(db.Model, UserMixin): #Table user
