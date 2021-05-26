e3bb9eaabf3fdd043c38e8f279016d44b360ee79
Thippi <37799874+MiuMiuMiuMiuMiu@users.noreply.github.com>
2021-05-26 18:38:54 +0200

    Fixat bug och korrigerat profilformuläret
    Bug från stora bokstäver till mindre bokstäver.

--- ThankYou_Kod/ThankYou/website/views.py
+++ ThankYou_Kod/ThankYou/website/views.py
@@ -214,30 +214,29 @@ def save_edit():
             flash('This email does not exist', category='error')
         elif len(fname) < 2:
             flash('First name must be greater than 2 characters.', category='error')
-        else:
-            if cpassword == '':
+        elif cpassword == '':
+            profile.email = cemail
+            profile.first_name = fname
+            profile.password = profile.password
+            db.session.commit()
+            flash('Profile updated!', category='success')
+        elif cpassword != '':
+            if len(cpassword) < 8:
+                flash('Password must be greater than 8 characters.', category='error')
+            elif not any(p.isupper() for p in cpassword): # Check if password includes at least one capital letter 
+                flash('Password must include at least one capital letter.', category='error')
+            elif not any(p.isdigit() for p in cpassword): # Check if password includes at least one number 
+                flash('Password must include at least one number.', category='error')
+            elif not any(char.islower() for char in cpassword):
+                flash('Password should have at least one lowercase letter', category='error')
+            elif not any(char.isdigit() for char in cpassword):
+                flash('Password should have at least one numeral', category='error')
+            else:
                 profile.email = cemail
                 profile.first_name = fname
-                profile.password = profile.password
+                profile.password = generate_password_hash(cpassword, method='sha256')#Hash password
                 db.session.commit()
                 flash('Profile updated!', category='success')
-            elif cpassword != '':
-                if len(cpassword) < 8:
-                    flash('Password must be greater than 8 characters.', category='error')
-                elif not any(p.isupper() for p in cpassword): # Check if password includes at least one capital letter 
-                    flash('Password must include at least one capital letter.', category='error')
-                elif not any(p.isdigit() for p in cpassword): # Check if password includes at least one number 
-                    flash('Password must include at least one number.', category='error')
-                elif not any(char.isupper() for char in cpassword):
-                    flash('Password should have at least one uppercase letter', category='error')
-                elif not any(char.isdigit() for char in cpassword):
-                    flash('Password should have at least one numeral', category='error')
-                else:
-                    profile.email = cemail
-                    profile.first_name = fname
-                    profile.password = generate_password_hash(cpassword, method='sha256')#Hash password
-                    db.session.commit()
-                    flash('Profile updated!', category='success')
     except:
         flash('This account already exists in Thank You', category='error')
     
